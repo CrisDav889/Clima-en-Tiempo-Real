de7f08739923a951c5268033d1627b39877000b0
CrisDav889 <145298706+CrisDav889@users.noreply.github.com>
2024-12-02 18:23:14 -0600

    Add files via upload

---
+++ API clima.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def obtener_clima(ciudad, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&lang=es&units=metric"
+    respuesta = requests.get(url)
+
+    if respuesta.status_code == 200:
+        datos = respuesta.json()
+        clima = datos['weather'][0]['description']
+        temp_actual = datos['main']['temp']
+        temp_min = datos['main']['temp_min']
+        temp_max = datos['main']['temp_max']
+
+        print(f"Clima en {ciudad}: {clima}")
+        print(f"Temperatura actual: {temp_actual}°C")
+        print(f"Temperatura mínima: {temp_min}°C")
+        print(f"Temperatura máxima: {temp_max}°C")
+    else:
+        print(f"Error al obtener el clima de {ciudad}. Código de estado: {respuesta.status_code}")
+
+
+if __name__ == "__main__":
+    ciudad = input("Ingrese la ciudad: ")
+    api_key = '27b9e60a27a40d331ab03b9638fc6f47'
+    obtener_clima(ciudad, api_key)
+
